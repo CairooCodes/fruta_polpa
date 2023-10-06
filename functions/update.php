8d0a07c198518aab7610d96cd96cd4a75d0c363a
WallaceNeryVeras <wallacenery.wn@gmail.com>
2023-10-06 17:47:41 -0300

    settings

--- functions/update.php
+++ functions/update.php
@@ -4,14 +4,14 @@ function updateBlog($id, $name, $img, $description, $link)
 {
   global $pdo;
   if ($img) {
-    $stmt = $pdo->prepare("UPDATE cursos SET name = :name, img=:img, description=:description, link=:link WHERE id = :id");
+    $stmt = $pdo->prepare("UPDATE blogs SET name = :name, img=:img, description=:description, link=:link WHERE id = :id");
     $stmt->bindParam(':name', $name);
     $stmt->bindValue(':img', $img);
     $stmt->bindParam(':description', $description);
     $stmt->bindParam(':link', $link);
     $stmt->bindParam(':id', $id);
   } else {
-    $stmt = $pdo->prepare("UPDATE cursos SET name = :name, description=:description, link=:link WHERE id = :id");
+    $stmt = $pdo->prepare("UPDATE blogs SET name = :name, description=:description, link=:link WHERE id = :id");
     $stmt->bindParam(':name', $name);
     $stmt->bindParam(':description', $description);
     $stmt->bindParam(':link', $link);
@@ -24,14 +24,14 @@ function updateProduct($id, $name, $img, $description, $price)
 {
   global $pdo;
   if ($img) {
-    $stmt = $pdo->prepare("UPDATE cursos SET name = :name, img=:img, description=:description, price=:price WHERE id = :id");
+    $stmt = $pdo->prepare("UPDATE products SET name = :name, img=:img, description=:description, price=:price WHERE id = :id");
     $stmt->bindParam(':name', $name);
     $stmt->bindValue(':img', $img);
     $stmt->bindParam(':description', $description);
     $stmt->bindParam(':price', $price);
     $stmt->bindParam(':id', $id);
   } else {
-    $stmt = $pdo->prepare("UPDATE cursos SET name = :name, description=:description, price=:price WHERE id = :id");
+    $stmt = $pdo->prepare("UPDATE products SET name = :name, description=:description, price=:price WHERE id = :id");
     $stmt->bindParam(':name', $name);
     $stmt->bindParam(':description', $description);
     $stmt->bindParam(':price', $price);
