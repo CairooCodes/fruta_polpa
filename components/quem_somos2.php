85e9c88a2447e668f4953bfd892a18a98331bdb4
WallaceNeryVeras <wallacenery.wn@gmail.com>
2023-10-10 17:53:23 -0300

    working who we are

---
+++ components/quem_somos2.php
@@ -0,0 +1,111 @@
+<section>
+    <div class="container">
+        <div id="intro">
+            <button>
+                <a href="./products.php">CONHEÇA NOSSOS PRODUTOS</a>
+            </button>
+        </div>
+        <div id="intro" class="d-flex flex-wrap align-items-center justify-content-between">
+            <div class="text-content col-md-6">
+                <div>
+                    <p><span style="font-size: 18px; color: #00284a; letter-spacing: 4px; font-weight: 300;">MAIS DE</span></p>
+                </div>
+                <div class="counter m-2 p-2" style="font-size: 42pt;" data-target="15" data-source="0" data-duration="3000" class="counter" counter="0">
+                    15
+                </div>
+                <div>
+                    <p>Trabalhamos com 25 sabores desde os mais tradicionais que são a acerola, goiaba e maracujá, como também contamos com a linha funcional, montada com 6 sabores especialmente pensadas para trazer benefícios diretos a saúde e qualidade das milhares de famílias em 7 estados diferentes: Piauí, Maranhão, Tocantins, Pará, Amapá, Goiás/DF e Ceará.</p>
+                    <a href="quem_somos.php">Leia mais</a>
+                </div>
+            </div>
+            <div class="col-md-6" style="max-width: 40%; height: auto; text-align: right;">
+                <img src="./assets/img/quemsomos.jpg" alt="quemsommos">
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- <div class="container-fluid">
+    <div class="botao1 botaoc col-xs-10 col-sm-12 col-md-12 col-xs-offset-1 col-sm-offset-0 col-md-offset-0" id="botaobanner"><a id="_428958716_1620157327" target="_self" class="col-xs-12 col-sm-12 col-md-12 col-xs-offset-0 col-sm-offset-0 col-md-offset-0 btn" href="#produtos"><i class="fa "></i>&nbsp;CONHEÇA NOSSOS PRODUTOS <i class="fas fa-angle-down"></i></a>
+    </div>
+    <style type="text/css">
+        .counter {
+            font-weight: 800;
+            color: #00284a;
+            font-size: 90px;
+            line-height: 0.7;
+        }
+
+        .counter:after {
+            content: ' anos sendo referência em tradição, qualidade e tecnologia.';
+            color: #00284a;
+            font-size: 47px;
+        }
+
+        @media (max-width: 767px) {
+            .counter:after {
+                font-size: 35px;
+            }
+
+            .counter {
+                font-size: 60px;
+                line-height: 0.7;
+            }
+        }
+
+        .zindex-2 {
+            z-index: -2;
+        }
+
+        #intro {
+            min-height: 600px;
+            display: flex;
+            flex-wrap: wrap;
+            align-items: center;
+        }
+
+        #intro2 {
+            position: relative;
+        }
+    </style>
+    <div class=" meia-largura-esq botaolinha col-xs-12 col-sm-7 col-md-7 col-xs-offset-0 col-sm-offset-0 col-md-offset-0" id="intro">
+        <div id="text_6503b080c101d" class="col-xs-12 col-sm-12 col-md-12 col-xs-offset-0 col-sm-offset-0 col-md-offset-0">
+            <p><span style="font-size: 20px; color: #00284a; letter-spacing: 4px; font-weight: 300;">MAIS DE</span></p>
+        </div>
+        <div id="_1147258240_1620157562" data-target="75" data-source="0" data-duration="3000" class="col-xs-12 col-sm-12 col-md-12 col-xs-offset-0 col-sm-offset-0 col-md-offset-0 counter " counter="0">75</div>
+        <div id="text_6503b080c144c" class="col-xs-12 col-sm-10 col-md-9 col-xs-offset-0 col-sm-offset-0 col-md-offset-0">
+            <p><span style="font-size: 12pt;">Em 1942, o imigrante italiano Giuseppe De Marchi deu início a partir da agricultura, a história do grupo De Marchi, conglomerado de empresas que atua desde o cultivo em grande escala de frutas, legumes e verduras, até a produção de uma ampla gama de alimentos congelados e comercialização de alimentos “in natura” para o Brasil e Exterior.</span></p>
+        </div>
+        <div class=" col-xs-12 col-sm-12 col-md-12 col-xs-offset-0 col-sm-offset-0 col-md-offset-0" href="/assets/img/quemsomos.jpg"><a href="/assets/img/quemsomos.jpg" target="_self" class="col-xs-12 col-sm-12 col-md-12 col-xs-offset-0 col-sm-offset-0 col-md-offset-0 btn" href="quem_somos.php"><i class="fa "></i>&nbsp;Leia mais</a>
+            <div class="clear"></div>
+        </div>
+        <div class="clear"></div>
+    </div>
+    <div class=" zindex-2 col-xs-12 col-sm-5 col-md-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-0" id="_5e7e6476e1e45_1585341558">
+        <div class="container-fluid  zindex-2 wow fadeInRight animated" name="Container_6503b080c18e5" id="_287120308_1620157562" style="visibility: visible; animation-name: fadeInRight;">
+            <div class="clear"></div>
+        </div>
+        <style type="text/css">
+            @media (min-width: 768px) and (max-width: 991px) {
+                #_5e7e6476e1e45_1585341558 {
+                    margin-left: -150px;
+                    width: calc(40% + 162px);
+                }
+            }
+        </style>
+        <div class="clear"></div>
+    </div>
+    <div class="clear"></div>
+</div>
+    <style type="text/css">
+        @media (min-width: 768px) and (max-width: 991px) {
+            #_5e7e6476e1e45_1585341558 {
+                margin-left: -150px;
+                width: calc(40% + 162px);
+            }
+        }
+    </style>
+    <div class="clear"></div>
+</div>
+</div> -->
--- home2.php
+++ home2.php
@@ -1,48 +1,308 @@
+<?php
+require "db_config.php";
+require "config/helper.php";
+require "config/url.class.php";
+require "./functions/get.php";
+
+$banners = getBanners();
+$products = getProducts();
+$blogs = getBlogs();
+$abouts = getAbouts();
+
+$URI = new URI();
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Inclua o link para o arquivo CSS do Tailwind CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css" rel="stylesheet">
-    <style>
-        /* Estilo para o efeito da linha laranja */
-        .nav-link {
-            position: relative;
-            text-decoration: none;
+<?php include "./components/heads.php"; ?>
+</head>
+
+<body>
+  <!-- Start block -->
+  <?php include "./components/navbar.php"; ?>
+  <?php include "./components/banners.php"; ?>
+  <?php include "./components/products.php"; ?>
+  <?php include "./components/quem_somos2.php"; ?>
+  <!-- <?php include "./components/blog.php"; ?> -->
+  <!-- <?php include "./components/form.php"; ?> -->
+
+
+  <!-- <section id="clientes" class="max-w-screen-xl px-4 py-8 mx-auto lg:py-24 lg:px-6">
+    <h3 class="text-2xl font-bold text-center">Clientes que confiam em nosso trabalho </h3>
+    <div class="pt-10">
+      <div class="grid grid-cols-3 gap-8">
+        <div class="p-10 shadow rounded-b-xl">
+          <img class="mb-2 w-56 h-full" src="./assets/img/clientes/adidas.jpg" alt="profile picture">
+        </div>
+        <div class="p-10 shadow rounded-b-xl">
+          <img class="mb-2 w-56 h-full" src="./assets/img/clientes/ART FARMA.png">
+        </div>
+        <div class="p-10 shadow rounded-b-xl">
+          <img class="mb-2 w-56 h-full" src="./assets/img/clientes/floriculturali.jpg">
+        </div>
+        <div class="p-10 shadow rounded-b-xl">
+          <img class="mb-2 w-56 h-full" src="./assets/img/clientes/Piauiense.png">
+        </div>
+        <div class="p-5 shadow rounded-b-xl">
+          <img class="mb-2" src="./assets/img/clientes/NorteLink.png">
+        </div>
+        <div class="p-5 shadow rounded-b-xl">
+          <img class="mb-2" src="./assets/img/clientes/cci.png">
+        </div>
+      </div>
+      <div class="swiper-pagination swiper-pacotes"></div>
+    </div>
+    <div id="#depoimentos" class="max-w-screen-xl lg:px-4 lg:py-8 mx-auto text-center lg:py-12 lg:px-6">
+      <div class="swiper mySwiper2 pt-20 pb-20">
+        <div class="swiper-wrapper pb-10">
+          <div class="swiper-slide h-60 lg:h-32 flex p-10 items-center rounded-b-xl">
+            <figure class="max-w-screen-md mx-auto">
+              <svg class="h-12 mx-auto mb-3 text-color1" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor" />
+              </svg>
+              <blockquote>
+                <p class="text-lg text-justify font-medium text-gray-900 md:text-2xl">"A Boom é uma parceira de peso para desenvolvimento da nossa marca no PI e MA através das redes sociais, nos ajudando a encantar os clientes, com estratégias assertivas e conteúdos que potencializam cada vez mais os nossos resultados! Sem dúvidas, a Boom faz parte do nosso melhor time de vendas!"</p>
+              </blockquote>
+              <figcaption class="flex items-center justify-center mt-6 space-x-3">
+                <img class="w-6 h-6 rounded-full" src="./assets/img/clientes/adidas.jpg" alt="profile picture">
+                <div class="flex items-center divide-x-2 divide-gray-500">
+                  <div class="pr-3 font-medium text-gray-900">Andréa Sales </div>
+                  <div class="pl-3 text-sm font-light text-color1">
+                    Supervisora Adidas</div>
+                </div>
+              </figcaption>
+            </figure>
+          </div>
+          <div class="swiper-slide h-60 lg:h-32 flex p-10 items-center rounded-b-xl">
+            <figure class="max-w-screen-md mx-auto">
+              <svg class="h-12 mx-auto mb-3 text-color1" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor" />
+              </svg>
+              <blockquote>
+                <p class="text-xl text-justify font-medium text-gray-900 md:text-2xl">"Somos clientes da Boom há aproximadamente 01 ano e estamos muito satisfeitos com a resultado da parceria. O número de seguidores do Instagram e as vendas por canais digitais aumentaram e temos resultado satisfatório com o tráfego pago."</p>
+              </blockquote>
+              <figcaption class="flex items-center justify-center mt-6 space-x-3">
+                <img class="w-6 h-6 rounded-full" src="./assets/img/clientes/Piauiense.png" alt="profile picture">
+                <div class="flex items-center divide-x-2 divide-gray-500">
+                  <div class="pr-3 font-medium text-gray-900">Lalinne Moura</div>
+                  <div class="pl-3 text-sm font-light text-color1">
+                    Proprietária Livraria Piauiense</div>
+                </div>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+        <div class="swiper-pagination swiper-pacotes"></div>
+      </div>
+    </div>
+  </section> -->
+
+  <?php include "./components/footer.php" ?>
+  <?php include "./components/modal_form_whats.php" ?>
+  <script src="assets/js/tw.js"></script>
+  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+  <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+	<script src="./assets/js/lz.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.js"></script>
+ <script>
+    const carousel3Dswiper = new Swiper(".carousel-3D-swiper", {
+    loop: true,
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 3,
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 350,
+      modifier: 1,
+      slideShadows: true
+    },
+		 navigation: {
+      nextEl: ".swiper-button-next-blog",
+      prevEl: ".swiper-button-prev-blog",
+    },
+    pagination: {
+      el: ".swiper-pagination-blog"
+    }
+  });
+ </script>
+	<script>
+		var swiper = new Swiper(".swiper_banners", {
+			loop: true,
+			autoplay: {
+				delay: 5000,
+				disableOnInteraction: false,
+			},
+			pagination: {
+				el: ".swiper-pagination-banners-main",
+				clickable: true,
+			},
+			navigation: {
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev",
+			},
+		});
+	</script>
+  <script>
+		var swiper = new Swiper(".swiper_products", {
+			loop: true,
+      freeMode: true,
+			breakpoints: {
+				300: {
+					slidesPerView: 1.3,
+					spaceBetween: 20,
+				},
+				640: {
+					slidesPerView: 2,
+					spaceBetween: 30,
+				},
+				768: {
+					slidesPerView: 3,
+					spaceBetween: 30,
+				},
+				1024: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+		});
+	</script>
+  <script>
+    $('a[href^="#"]').on('click', function(e) {
+      e.preventDefault();
+      var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top;
+
+      $('html, body').animate({
+        scrollTop: targetOffset - 100
+      }, 500);
+    });
+  </script>
+  <script>
+    var swiper = new Swiper(".mySwiper", {
+      autoplay: {
+        delay: 3000, // Time between slides in milliseconds (3 seconds)
+        disableOnInteraction: false, // Autoplay continues even when the user interacts with the swiper
+      },
+      freeMode: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      },
+    });
+  </script>
+  <script>
+    var swiper = new Swiper(".mySwiper2", {
+      freeMode: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 30,
+        },
+        1024: {
+          slidesPerView: 1,
+          spaceBetween: 40,
+        },
+      },
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: true,
+      },
+      loop: true,
+    });
+  </script>
+  <script>
+    let prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+      const currentScrollPos = window.pageYOffset;
+      const navbar = document.getElementById("navbar");
+
+      if (prevScrollpos > currentScrollPos) {
+        navbar.classList.add("active");
+      } else {
+        navbar.classList.remove("active");
+      }
+
+      prevScrollpos = currentScrollPos;
+    }
+  </script>
+  <script>
+    const cards = document.querySelectorAll('.fade-in-up');
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.2
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+
+    cards.forEach(card => observer.observe(card));
+  </script>
+</body>
+
+</html>
+
+<script>
+// Obtém todos os elementos com a classe "counter"
+var counters = document.querySelectorAll('.counter');
+
+// Função para animar a contagem
+function countUp() {
+    counters.forEach(counter => {
+        var target = parseInt(counter.getAttribute('data-target')); // Obtém o valor alvo do atributo data-target
+        var duration = parseInt(counter.getAttribute('data-duration')); // Obtém a duração da animação do atributo data-duration
+        var source = parseInt(counter.getAttribute('data-source')) || 0; // Obtém o valor de origem do atributo data-source, padrão é 0
+        var increment = Math.ceil(target / (duration / 100)); // Calcula o incremento com base na duração
+
+        // Função de animação
+        function animate() {
+            var currentCount = parseInt(counter.innerText);
+            if (currentCount < target) {
+                counter.innerText = Math.min(currentCount + increment, target);
+                setTimeout(animate, 10);
+            } else {
+                counter.innerText = target;
+            }
         }
 
-        .nav-link:hover::after {
-            content: "";
-            position: absolute;
-            left: 0;
-            right: 0;
-            bottom: -4px;
-            height: 2px;
-            background-color: orange;
-        }
-    </style>
-    <title>Sua Página</title>
-</head>
-<body>
-    <nav class="bg-orange-800 text-black py-4">
-        <div class="container mx-auto flex justify-between items-center">
-            <div class="text-2xl font-bold">Logo</div>
-            <ul class="flex space-x-4">
-                <li>
-                    <a href="#" class="nav-link">Página Inicial</a>
-                </li>
-                <li>
-                    <a href="#" class="nav-link">Sobre</a>
-                </li>
-                <li>
-                    <a href="#" class="nav-link">Serviços</a>
-                </li>
-                <li>
-                    <a href="#" class="nav-link">Contato</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-</body>
-</html>
+        // Inicia a animação
+        animate();
+    });
+}
+
+// Chama a função de contagem quando a janela é carregada
+window.addEventListener('load', countUp);
+</script>
