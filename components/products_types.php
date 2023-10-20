7efea3ee30fdc5dabf444a096f3bbe77b443db48
tiagoCB41 <thiagocarvalho041@gmail.com>
2023-10-19 21:19:56 -0300

    hovering

--- components/products_types.php
+++ components/products_types.php
@@ -3,7 +3,7 @@
         <h6 class="lg:text-3xl text-xl pb-2 text-center "><span style="font-size: 20px;letter-spacing: 4px; font-weight: 300;" class="text-color1 uppercase bg-clip-text bg-gradient-to-r from-color2 to-color2">Nossas principais</span></h6>
         <h1 class="lg:text-6xl text-5xl pb-4 text-center mb-4"><span style="font-size: 60px; letter-spacing: 0.5px; font-family: 'Poppins';" class="font-black text-color1 bg-clip-text bg-gradient-to-r from-color2 to-color2">Linhas de Produtos</span></h1>
         <div class="swiper-wrapper pb-4 pt-12">
-            <div class="swiper-slide grid bg-white rounded-2xl shadow2 relative">
+            <div class="swiper-slide hover:-translate-y-4 transition duration-500 grid bg-white rounded-2xl shadow2 relative">
                 <img class='lazy object-cover h-full px-3 relative top-[-50px]' data-src='https://demarchi.com.br/images/gusto-e-salute/gusto-home.png'>
                 <div style="background-image: url(<?php echo $URI->base("/assets/img/gusto-fundo.jpg"); ?>)" class="rounded-b-2xl">
                     <div class="flex justify-center pb-5">
@@ -19,10 +19,10 @@
                 </div>
             </div>
 
-            <div class="swiper-slide grid bg-white rounded-2xl shadow2 relative">
+            <div class="swiper-slide hover:-translate-y-4 transition duration-500 grid bg-white rounded-2xl shadow2 relative">
                 <img class='lazy object-cover h-full px-3 relative top-[-50px]' data-src='https://demarchi.com.br/images/gusto-e-salute/gusto-home.png'>
                 <div style="background-image: url(<?php echo $URI->base("/assets/img/gusto-fundo.jpg"); ?>)" class="rounded-b-2xl">
-                    <div class="flex justify-evenly pb-5">
+                    <div class="flex justify-center pb-5">
                         <a href="<?php echo $URI->base('/product/teste.php'); ?>">
                             <h3 class="text-xl  text-gray-900 text-center font pt-6 mb-2" style="font-family: 'Poppins';">100% naturais</h3>
                             <h3 class="text-4xl text-white text-center font-black mb-2">Polpas</h3>
