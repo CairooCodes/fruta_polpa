c0259f830dba96fc9c5a163b459f3dbb2402dee3
AleexBrendon <alexbrendo.dev@gmail.com>
2023-10-31 22:47:09 -0300

    ajust responsive institucional fruta polpa

--- components/footer.php
+++ components/footer.php
@@ -4,8 +4,7 @@
     <div class="flex-shrink-0 w-64 mx-auto text-center md:mx-0 md:text-left">
       <div>
         <a href="<?php echo $URI->base("/home"); ?>" class="flex justify-center">
-          <img data-src="<?php echo $URI->base("/assets/img/$logo"); ?>" class="logo lazy" alt="" />
-          <!-- <img src="./assets/img/logo.png" alt="FrutaPolpaLogo" class="w-2xl h-auto mx-auto md:mx-0"> -->
+          <img data-src="<?php echo $URI->base("./assets/img/$logo"); ?>" class="logo lazy" alt="<?php echo $title; ?>"/>
         </a>
       </div>
       <div class="text-center mt-2">
--- institucional.php
+++ institucional.php
@@ -22,8 +22,8 @@ $URI = new URI();
 
 <body>
 	<?php include "./components/navbar.php"; ?>
-	<div class="pt-5">
-		<div class="pb-24">
+	<section class="max-w-full px-4 pt-5 mx-auto lg:grid grid-cols-2">
+		<div class="mx-auto pb-8">
 			<div class="text-center">
 				<p class="text-2xl text-orange-600 uppercase font-sans">Nossa História</p>
 				<h1 class="font-bold text-orange-600 text-5xl font-sans mb-2">
@@ -33,127 +33,153 @@ $URI = new URI();
 		</div>
 		<div class="flex flex-col sm:flex-row justify-between md:max-w-6xl mx-auto">
 			<div class="mb-4 sm:mr-4">
-				<!-- <div class="rounded-full bg-color2 text-sm w-32 text-black font-medium mb-4 text-center mt-9">INÍCIO</div> -->
 				<div class="grid justify-start">
 					<div class="flex">
-						<img src="./assets/img/icone linha do tempo.png" class="mr-2">
+						<div class="grid grid-rows-2 gap-2 mr-2 p-2">
+							<i class="text-center bi bi-record-circle"></i>
+							<i class="bi bi-dot"></i>
+							<i class="bi bi-dot"></i>
+							<i class="bi bi-dot"></i>
+							<i class="bi bi-dot"></i>
+							<i class="bi bi-dot"></i>
+						</div>
 						<div class="grid justify-start">
 							<p class="text-gray-500">(2003)<br><span class="font-semibold font-sans text-color1">Fundação</span></p>
 							<p class="font-sans">Fundada no ano de 2003, a Fruta Polpa iniciou suas atividades<br>
-												vendendo frutas in natura na cidade de Teresina.<br>
-												Com apenas 04 colaboradores, começou o negócio de <br>produção de polpas.</p>
+								vendendo frutas in natura na cidade de Teresina.<br>
+								Com apenas 04 colaboradores, começou o negócio de <br>produção de polpas.</p>
 						</div>
 					</div>
 					<br>
 					<div class="flex">
-						<img src="./assets/img/icone linha do tempo.png" class="mr-2">
+						<div class="grid grid-rows-2 gap-2 mr-2 p-2">
+							<i class="text-center bi bi-record-circle"></i>
+							<i class="bi bi-dot"></i>
+							<i class="bi bi-dot"></i>
+							<i class="bi bi-dot"></i>
+							<i class="bi bi-dot"></i>
+							<i class="bi bi-dot"></i>
+						</div>
 						<div class="grid justify-start">
 							<p class="text-gray-500">(2010)<br><span class="font-semibold font-sans text-color1">Expanção Nacional</span></p>
 							<p class="font-sans">Fundada no ano de 2003, a Fruta Polpa iniciou suas atividades<br>
-												vendendo frutas in natura na cidade de Teresina.<br>
-												Com apenas 04 colaboradores, começou o negócio de <br>produção de polpas.</p>
+								vendendo frutas in natura na cidade de Teresina.<br>
+								Com apenas 04 colaboradores, começou o negócio de <br>produção de polpas.</p>
 						</div>
 					</div>
 				</div>
 			</div>
 			<div class="grid justify-items-end">
-				<!-- <div class="rounded-full bg-color2 text-sm w-32 h-5 text-black font-medium mb-4 text-center mt-7">ATUALMENTE</div> -->
 				<div class="text-right img-right">
 					<div class="flex">
 						<div class="grid justify-start">
 							<p class="text-gray-500">(2015)<br><span class="font-semibold font-sans text-color1">Lançamento de Novos Produtos</span></p>
 							<p class="font-sans"><br>Fundada no ano de 2003, a Fruta Polpa iniciou suas atividades<br>
-												vendendo frutas in natura na cidade de Teresina.<br>
-												Com apenas 04 colaboradores, começou o negócio de <br>produção de polpas.</p>
+								vendendo frutas in natura na cidade de Teresina.<br>
+								Com apenas 04 colaboradores, começou o negócio de <br>produção de polpas.</p>
 						</div>
-						<img src="./assets/img/icone linha do tempo.png" class="ml-2">
+						<div class="grid grid-rows-2 gap-2 mr-2 p-2">
+							<i class="text-center bi bi-record-circle"></i>
+							<i class="bi bi-dot"></i>
+							<i class="bi bi-dot"></i>
+							<i class="bi bi-dot"></i>
+							<i class="bi bi-dot"></i>
+							<i class="bi bi-dot"></i>
+						</div>
 					</div>
 					<br>
 					<div class="flex">
 						<div class="grid justify-start">
 							<p class="text-gray-500">(2018)<br><span class="font-semibold font-sans text-color1">Parceria Global</span></p>
 							<p class="font-sans"><br>Fundada no ano de 2003, a Fruta Polpa iniciou suas atividades<br>
-												vendendo frutas in natura na cidade de Teresina.<br>
-												Com apenas 04 colaboradores, começou o negócio de <br>produção de polpas.</p>
+								vendendo frutas in natura na cidade de Teresina.<br>
+								Com apenas 04 colaboradores, começou o negócio de <br>produção de polpas.</p>
 						</div>
-						<img src="./assets/img/icone linha do tempo.png" class="ml-2">
+						<div class="grid grid-rows-2 gap-2 mr-2 p-2">
+							<i class="text-center bi bi-record-circle"></i>
+							<i class="bi bi-dot"></i>
+							<i class="bi bi-dot"></i>
+							<i class="bi bi-dot"></i>
+							<i class="bi bi-dot"></i>
+							<i class="bi bi-dot"></i>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<div class="mx-auto max-w-6xl pt-2 about mt-24">
-		<section>
-			<div class="lg:pt-4 lg:p-0 lg:p-10 p-5">
-				<div class="md:grid md:grid-cols-2 md:gap-6">
-					<div class="mx-auto">
-						<h1 class="font-extrabold text-orange-600 text-4xl font-sans uppercase pb-3 lg:py-10">Fazendas</h1>
-						<p class="text-medium font-sans">A qualidade dos produtos "in natura" e congelados De Marchi tem origem nas fazendas do grupo e também dos produtores parceiros. Elas são a garantia de produtos cultivados com muito cuidado e sabor incomparáveis.</p>
-					</div>
-					<!-- Your HTML Structure -->
-					<div class="owl-carousel">
-						<div class="item">
-							<img src="./assets/img/fazenda.png" class="rounded-lg lg:h-80 cover" alt="fazenda">
-						</div>
-						<div class="item">
-							<img src="./assets/img/fazenda2.png" class="rounded-lg lg:h-80 cover" alt="fazenda2">
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-
-		<section>
-			<div class="lg:pt-4 lg:p-0 lg:p-10 p-5">
-				<div class="md:grid md:grid-cols-2 md:gap-6">
-
-					<div class="mx-auto">
-						<h1 class="font-extrabold text-orange-600 text-4xl font-sans uppercase pb-3 lg:py-10">Indústria</h1>
-						<p class="text-medium font-sans ">O grupo conta com mais de 3.000 profissionais altamente capacitados e modernos equipamentos para garantir produtos minimamente processados, mantendo toda qualidade e sabor que vem direto do campo.</p>
-					</div>
-
-					<!-- Your HTML Structure -->
-					<div class="owl-carousel order-first">
-						<div class="item">
-							<img src="./assets/img/industria2.png" class="rounded-lg lg:h-80 cover" alt="industria">
-						</div>
-						<div class="item">
-							<img src="./assets/img/industria.png" class="rounded-lg lg:h-80 cover" alt="industria2">
-						</div>
-					</div>
-
-				</div>
-			</div>
-		</section>
-
-		<section>
-			<div class="lg:pt-4 lg:p-0 lg:p-10 p-5">
-				<div class="md:grid md:grid-cols-2 md:gap-6">
-					<div class="mx-auto">
-						<h1 class="font-extrabold text-orange-600 text-4xl font-sans uppercase pb-3 lg:py-10">Supermercados</h1>
-						<p class="text-medium font-sans">A De Marchi abastece, diariamente, centenas de supermercados por todo país e também administra as seções FLV (Frutas, Legumes e Verduras) de renomadas redes de supermercados.</p>
-					</div>
-					<!-- Your HTML Structure -->
-					<div class="owl-carousel">
-						<div class="item">
-							<img src="./assets/img/supermercado.png" class="rounded-lg" alt="supermercado">
-						</div>
-						<div class="item">
-							<img src="./assets/img/supermercado2.png" class="rounded-lg" alt="supermercado2">
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-		<div class="mb-10">
-			<a href="<?php echo $URI->base("distribuidores"); ?>" class="flex justify-center">
-				<button class="middle none center rounded-lg bg-orange-500 py-5 px-4 shadow-md shadow-orange-500/20 transition-all hover:shadow-lg hover:shadow-orange-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none rounded-full font-sans text-white uppercase font-bold">
-					<span class="pr-2">Encontre o Distribuidor mais próximo</span><i class="bi bi-chevron-right"></i>
-				</button>
-			</a>
 		</div>
-	</div>
-	</div>
+		<div class="mx-auto max-w-6xl pt-2 about mt-24">
+			<section>
+				<div class="lg:pt-4 lg:p-0 lg:p-10 p-5">
+					<div class="md:grid md:grid-cols-2 md:gap-6">
+						<div class="mx-auto">
+							<h1 class="font-extrabold text-orange-600 text-4xl font-sans uppercase pb-3 lg:py-10">Fazendas</h1>
+							<p class="text-medium font-sans">A qualidade dos produtos "in natura" e congelados De Marchi tem origem nas fazendas do grupo e também dos produtores parceiros. Elas são a garantia de produtos cultivados com muito cuidado e sabor incomparáveis.</p>
+						</div>
+						<!-- Your HTML Structure -->
+						<div class="owl-carousel">
+							<div class="item">
+								<img src="./assets/img/fazenda.png" class="rounded-lg lg:h-80 cover" alt="fazenda">
+							</div>
+							<div class="item">
+								<img src="./assets/img/fazenda2.png" class="rounded-lg lg:h-80 cover" alt="fazenda2">
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section>
+				<div class="lg:pt-4 lg:p-0 lg:p-10 p-5">
+					<div class="md:grid md:grid-cols-2 md:gap-6">
+
+						<div class="mx-auto">
+							<h1 class="font-extrabold text-orange-600 text-4xl font-sans uppercase pb-3 lg:py-10">Indústria</h1>
+							<p class="text-medium font-sans ">O grupo conta com mais de 3.000 profissionais altamente capacitados e modernos equipamentos para garantir produtos minimamente processados, mantendo toda qualidade e sabor que vem direto do campo.</p>
+						</div>
+
+						<!-- Your HTML Structure -->
+						<div class="owl-carousel order-first">
+							<div class="item">
+								<img src="./assets/img/industria2.png" class="rounded-lg lg:h-80 cover" alt="industria">
+							</div>
+							<div class="item">
+								<img src="./assets/img/industria.png" class="rounded-lg lg:h-80 cover" alt="industria2">
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</section>
+
+			<section>
+				<div class="lg:pt-4 lg:p-0 lg:p-10 p-5">
+					<div class="md:grid md:grid-cols-2 md:gap-6">
+						<div class="mx-auto">
+							<h1 class="font-extrabold text-orange-600 text-4xl font-sans uppercase pb-3 lg:py-10">Supermercados</h1>
+							<p class="text-medium font-sans">A De Marchi abastece, diariamente, centenas de supermercados por todo país e também administra as seções FLV (Frutas, Legumes e Verduras) de renomadas redes de supermercados.</p>
+						</div>
+						<!-- Your HTML Structure -->
+						<div class="owl-carousel">
+							<div class="item">
+								<img src="./assets/img/supermercado.png" class="rounded-lg" alt="supermercado">
+							</div>
+							<div class="item">
+								<img src="./assets/img/supermercado2.png" class="rounded-lg" alt="supermercado2">
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<div class="mb-10">
+				<a href="<?php echo $URI->base("distribuidores"); ?>" class="flex justify-center">
+					<button class="middle none center rounded-lg bg-orange-500 py-5 px-4 shadow-md shadow-orange-500/20 transition-all hover:shadow-lg hover:shadow-orange-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none rounded-full font-sans text-white uppercase font-bold">
+						<span class="pr-2">Encontre o Distribuidor mais próximo</span><i class="bi bi-chevron-right"></i>
+					</button>
+				</a>
+			</div>
+		</div>
+	</section>
 	<?php include "./components/footer.php"; ?>
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.js"></script>
