17451d9b5b1900c7a216c91b925866ce2332f94e
Cairo <cairofelipedev@gmail.com>
2024-02-09 14:23:35 -0300

    heads pl

--- components/heads.php
+++ components/heads.php
@@ -1,31 +1,29 @@
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Fruta Polpa</title>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Fruta Polpa</title>
 
-  <!-- Meta SEO -->
-  <meta name="title" content="Fruta Polpa">
-  <meta name="description" content="Fruta Polpa">
-  <meta name="robots" content="index, follow">
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <meta name="language" content="Portuguese">
-  <meta name="author" content="Cairo dev">
+<!-- Meta SEO -->
+<meta name="title" content="Fruta Polpa">
+<meta name="description" content="Fruta Polpa">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="Portuguese">
+<meta name="author" content="Cairo Codes">
 
-  <!-- Social media share -->
-  <meta property="og:title" content="Fruta Polpa">
-  <meta property="og:site_name" content="">
-  <meta property="og:url" content="https://frutapolpa.com.br/">
-  <meta property="og:description" content="Fruta Polpa">
-  <meta property="og:image" content="http://br220.teste.website/~cairod86/frutapolpa/assets/img/logo.png">
-  <!-- Favicon -->
-  <link rel="apple-touch-icon" sizes="180x180" href="./assets/img/logo.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $URI->base("/assets/img/icon.png"); ?>">
-  <script src="https://cdn.tailwindcss.com"></script>
-  <link rel="stylesheet" href="<?php echo $URI->base("/assets/css/style.css"); ?>">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.css" rel="stylesheet" />
-  <script src="https://cdn.jsdelivr.net/npm/intersection-observer@0.12.2/intersection-observer.min.js"></script>
-  <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-  <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-</head>
+<!-- Social media share -->
+<meta property="og:title" content="Fruta Polpa">
+<meta property="og:site_name" content="https://frutapolpa.com.br">
+<meta property="og:url" content="https://frutapolpa.com.br">
+<meta property="og:description" content="Fruta Polpa">
+<meta property="og:image" content="https://frutapolpa.com.br/assets/img/logo.png">
+<!-- Favicon -->
+<link rel="apple-touch-icon" sizes="180x180" href="https://frutapolpa.com.br/assets/img/logo.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://frutapolpa.com.br/assets/img/icon.png">
+<script src="https://cdn.tailwindcss.com"></script>
+<link rel="stylesheet" href="https://frutapolpa.com.br/assets/css/style.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/intersection-observer@0.12.2/intersection-observer.min.js"></script>
+<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
